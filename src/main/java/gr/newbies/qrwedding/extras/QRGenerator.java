83f1ba6fdb264210627919242defed8b78920d5e
newbi12 <shinigamidxd@gmail.com>
2016-04-17 06:22:50 +0300

    Test for everything

--- src/main/java/gr/newbies/qrwedding/extras/QRGenerator.java
+++ src/main/java/gr/newbies/qrwedding/extras/QRGenerator.java
@@ -19,11 +19,15 @@ public class QRGenerator {
     public String generateQR(String UUID_Visitor, String UUID_Event){	
 	String qrCodeData = ProgPaths.IMAGE_ONLINE_PATH.getData(UUID_Visitor,"/"); // to link edw
         
+        System.out.println("UUID_VIS->" + UUID_Visitor + "   UUID_EV-> " + UUID_Event );
         pathGenerator(UUID_Event);
+        System.out.println("After path generator");
         
         String filePath = ProgPaths.IMAGES_OUTPUT_FOLDER
-                .getData(UUID_Event,UUID_Visitor,"\\")
-                ;
+                .getData(UUID_Event,UUID_Visitor,"\\");
+        
+        System.out.println("Filepath -> " + filePath);
+        
         String charset = "UTF-8"; 
         Map hintMap = new HashMap();
         hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
@@ -40,9 +44,12 @@ public class QRGenerator {
     private boolean pathGenerator(String filePath){
         File newFile = new File(ProgPaths.IMAGES_OUTPUT_FOLDER
                 .getData(filePath,"\\"));
+        System.out.println("newFile -> " + newFile);
         if (!newFile.getParentFile().exists()){
+            System.out.println("Was it true?");
              newFile.getParentFile().mkdirs(); 
         } else {
+            System.out.println("Was false");
             return false;
         }
         return true;
@@ -52,9 +59,12 @@ public class QRGenerator {
             String charset, Map hintMap, int qrCodeheight, int qrCodewidth)
                             throws WriterException, IOException {
         filePath = filePath + ".png";
+        System.out.println("filePath for the second time -> " + filePath);
+        
         BitMatrix matrix = new MultiFormatWriter().encode( new String(
                 qrCodeData.getBytes(charset), charset), BarcodeFormat.QR_CODE,
                 qrCodewidth, qrCodeheight, hintMap);
+        
         MatrixToImageWriter.writeToFile(matrix, filePath
                 .substring(filePath.lastIndexOf('.') + 1), new File(filePath));
     }
