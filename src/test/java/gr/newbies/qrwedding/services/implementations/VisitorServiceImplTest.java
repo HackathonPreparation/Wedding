a1d9bac06c53fb10f64417d5958341b1d441c807
Alodapos <kazi666@hotmail.gr>
2016-04-17 13:56:19 +0300

    more tests

--- src/test/java/gr/newbies/qrwedding/services/implementations/VisitorServiceImplTest.java
+++ src/test/java/gr/newbies/qrwedding/services/implementations/VisitorServiceImplTest.java
@@ -2,20 +2,36 @@ package gr.newbies.qrwedding.services.implementations;
 
 import gr.newbies.qrwedding.models.dtos.VisitorCreationDTO;
 import gr.newbies.qrwedding.models.entities.Visitor;
+import gr.newbies.qrwedding.repositories.EventRepository;
+import gr.newbies.qrwedding.repositories.VisitorRepository;
+import gr.newbies.qrwedding.services.EventService;
+import gr.newbies.qrwedding.services.VisitorService;
 import org.json.simple.JSONArray;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 import static org.junit.Assert.*;
 
-/**
- *
- * @author SoUlKeePeR
- */
+@WebAppConfiguration
 public class VisitorServiceImplTest {
-    
+
+    @Mock
+    private VisitorRepository repository;
+
+    @InjectMocks
+    private VisitorServiceImpl mockService;
+
+    @Autowired
+    private VisitorService service;
+
     public VisitorServiceImplTest() {
     }
     
@@ -29,10 +45,10 @@ public class VisitorServiceImplTest {
     
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
     
     @After
     public void tearDown() {
     }
-
 }
